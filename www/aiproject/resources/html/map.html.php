fa474d2384568b7c1d8504c6ecaf6f1747878429
koktajlik <wera.oliwia3@gmail.com>
2023-01-17 17:39:09 +0100

    Added html skeleton structure for map site

---
+++ www/aiproject/resources/html/map.html.php
@@ -0,0 +1,13 @@
+<?php
+$imageSrc = "";
+?>
+<h1>Mapa</h1>
+<!--
+    TODO: Add leaflet map
+-->
+<p>Który budynek?</p>
+<!--
+    TODO: Add image containing 2 buildings
+ -->
+<button type="button" value="WI1">Budynek 1</button>
+<button type="button" value="WI2">Budynek 2</button>
