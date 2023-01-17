5be404cea41426f9652e8f988711dfc3f04b6ff0
koktajlik <wera.oliwia3@gmail.com>
2023-01-17 18:25:35 +0100

    Added login panel

---
+++ www/aiproject/resources/html/adminpanel.html.php
@@ -0,0 +1,8 @@
+<h1>Panel Logowania</h1>
+<form action="">
+    <label for="login">Login:</label>
+    <input type="text" id="login"><br>
+    <label for="password">Hasło:</label>
+    <input type="password" id="password"><br>
+    <input type="submit" value="Zaloguj">
+</form>
