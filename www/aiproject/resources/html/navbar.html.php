4ecd20c23f6ae1d9ecd1ed85a9bfa0befb77a892
Neikar <32700210+Neikar@users.noreply.github.com>
2023-01-18 20:13:17 +0100

    Update navbar.html.php

--- www/aiproject/resources/html/navbar.html.php
+++ www/aiproject/resources/html/navbar.html.php
@@ -1,7 +1,11 @@
-<nav>
-	<ul>
-		<li><a href="">&#191Donde?</a></li>
-		<li><a href="">Mapa</a></li>
-		<li><a href="">Szukaj</a></li>
-	</ul>
-<nav>
+<body>
+
+
+	<nav class="col-12 ">
+		<ul>
+			<li class="col-1 col-s-1"><a href="">&#191Donde?</a></li>
+			<li class="col-1 col-s-1"><a href="">Mapa</a></li>
+			<li class="col-1 col-s-1"><a href="">Plan</a></li>
+			<li class="col-1 col-s-1"><a href="">Szukaj</a></li>
+		</ul>
+	</nav>
