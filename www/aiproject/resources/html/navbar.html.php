de49ca3d479b42037c06118ba9b9f41eb2183b2a
Michal Dariusz Nowakowski <michaldariusznowakowski@protonmail.com>
2023-01-21 18:59:01 +0100

    Navbar fix, changed <A> to form

--- www/aiproject/resources/html/navbar.html.php
+++ www/aiproject/resources/html/navbar.html.php
@@ -2,10 +2,23 @@
 
 
 	<nav class="col-12 ">
+	<form action="index.php" method="post">
 		<ul>
-			<li class="col-1 col-s-1"><a href="">&#191Donde?</a></li>
-			<li class="col-1 col-s-1"><a href="">Mapa</a></li>
-			<li class="col-1 col-s-1"><a href="">Plan</a></li>
-			<li class="col-1 col-s-1"><a href="">Szukaj</a></li>
+			<li class="col-1 col-s-1">
+					<input hidden="true" type="submit" name="page" value="MainPage" id="MainPage">
+					<label for="MainPage">&#191Donde?</label>
+			</li>
+			<li class="col-1 col-s-1">
+					<input hidden="true" type="submit" name="page" value="Map" id="Map">
+					<label for="Map">Mapa</label>
+			<li class="col-1 col-s-1">
+					<input hidden="true" type="submit" name="page" value="Plan" id="Plan">
+					<label for="Search">Plan</label>	
+			</li>
+			<li class="col-1 col-s-1">
+					<input hidden="true" type="submit" name="page" value="Search" id="Search">
+					<label for="Search">Szukaj</label>
+			</li>
 		</ul>
+		</form>
 	</nav>
