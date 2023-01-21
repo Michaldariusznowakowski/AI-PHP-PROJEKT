525c293044665e1331a7124394d4fbb43ced6aed
Michal Dariusz Nowakowski <michaldariusznowakowski@protonmail.com>
2023-01-21 20:11:48 +0100

    Fixed resposive navbar

--- www/aiproject/resources/html/navbar.html.php
+++ www/aiproject/resources/html/navbar.html.php
@@ -8,11 +8,11 @@
 					<input hidden="true" type="submit" name="page" value="MainPage" id="MainPage">
 					<label for="MainPage">&#191Donde?</label>
 			</li>
-			<li class="col-1 col-s-1">
+			<li class="col-1 col-s-2">
 					<input hidden="true" type="submit" name="page" value="Map" id="Map">
 					<label for="Map">Mapa</label>
 			</li>
-			<li class="col-1 col-s-1">
+			<li class="col-1 col-s-3">
 					<input hidden="true" type="submit" name="page" value="Search" id="Search">
 					<label for="Search">Szukaj</label>
 			</li>
