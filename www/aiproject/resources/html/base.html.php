0c912020be98afc3eced6c89ad0753840773c99f
Michal Dariusz Nowakowski <michaldariusznowakowski@protonmail.com>
2023-01-21 20:11:29 +0100

    Added custom.css

--- www/aiproject/resources/html/base.html.php
+++ www/aiproject/resources/html/base.html.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../resources/css/style.css">
-    <title><?= $title ?? 'Donde' ?></title>
+    <link rel="stylesheet" href="../resources/css/custom.css">
+    <title><?= $HTML_PAGE_NAME ?? 'Donde' ?></title>
 </head>
-<body <?= isset($bodyClass) ? "class='$bodyClass'" : '' ?>>
+<body <?= isset($HTML_PAGE_NAME) ? "class='$HTML_PAGE_NAME'" : '' ?>>
 <?php require(__DIR__ . DIRECTORY_SEPARATOR . 'navbar.html.php') ?>
 <main><?= $main ?? null ?></main>
 <?php require(__DIR__ . DIRECTORY_SEPARATOR . 'footer.html.php') ?>
