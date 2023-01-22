b442a622eaadeb0e1950a3a9bf5192ab819ccd41
Neikar <32700210+Neikar@users.noreply.github.com>
2023-01-22 22:13:55 +0100

    Update footer.html.php

--- www/aiproject/resources/html/footer.html.php
+++ www/aiproject/resources/html/footer.html.php
@@ -3,7 +3,7 @@ $imgSrc = '../resources/img/WIZUT.svg';
 ?>
 
     <footer class="col-12 ">
-		<img class="col-1" src="<?=$imgSrc?>" alt="ZUT logo">
+		<img height="40px" class="col-1" src="<?=$imgSrc?>" alt="ZUT logo">
 		<p class="col-2" >&#169 Zachodniopomorski Uniwersytet Technologiczny</p>
 		<div class="col-1">
 			<form action="index.php" method="post">
