a806962be0083a2786d5dd51764a3c42166bce6d
koktajlik <wera.oliwia3@gmail.com>
2023-01-26 17:54:45 +0100

    Deleted usage of rooms coordinates

--- www/aiproject/php/Model/Database/Rooms.php
+++ www/aiproject/php/Model/Database/Rooms.php
@@ -6,10 +6,6 @@ class Rooms{
     private ?string $roomNumber = null;
     private ?int $roomType = null;
     private ?int $floorID = null;
-    private ?float $x = null;
-    private ?float $y = null;
-    private ?float $x2 = null;
-    private ?float $y2 = null;
 
     public function getRoomID(): ?int{
         return $this->roomID;
@@ -47,42 +43,6 @@ class Rooms{
         return $this;
     }
 
-    public function getX(): ?float{
-        return $this->x;
-    }
-
-    public function setX(?float $x) : Rooms{
-        $this->x = $x;
-        return $this;
-    }
-
-    public function getY(): ?float{
-        return $this->y;
-    }
-
-    public function setY(?float $y) : Rooms{
-        $this->y = $y;
-        return $this;
-    }
-
-    public function getX2(): ?float{
-        return $this->x2;
-    }
-
-    public function setX2(?float $x2) : Rooms{
-        $this->x2 = $x2;
-        return $this;
-    }
-
-    public function getY2(): ?float{
-        return $this->y2;
-    }
-
-    public function setY2(?float $y2) : Rooms{
-        $this->y2 = $y2;
-        return $this;
-    }
-
     public static function fromArray($array) : Rooms
     {
         $post = new self();
@@ -103,18 +63,6 @@ class Rooms{
         }
         if (isset($array['Pietra_idPietra'])) {
             $this->setFloorID($array['Pietra_idPietra']);
-        }
-        if (isset($array['X'])) {
-            $this->setX($array['X']);
-        }
-        if (isset($array['Y'])) {
-            $this->setY($array['Y']);
-        }
-        if (isset($array['X2'])) {
-            $this->setX2($array['X2']);
-        }
-        if (isset($array['Y2'])) {
-            $this->setY2($array['Y2']);
         }
 
         return $this;
@@ -157,31 +105,23 @@ class Rooms{
         $pdo = new Database();
         $pdo = $pdo->dbConnection();
         if (! $this->getRoomID()) {
-            $sql = "INSERT INTO pokoje (NumerPokoju, TypPokoju, Pietra_idPietra, X, Y, X2, Y2) VALUES (:NumerPokoju, :TypPokoju, :Pietra_idPietra, :X, :Y, :X2, :Y2)";
+            $sql = "INSERT INTO pokoje (NumerPokoju, TypPokoju, Pietra_idPietra) VALUES (:NumerPokoju, :TypPokoju, :Pietra_idPietra)";
             $statement = $pdo->prepare($sql);
             $statement->execute([
                 'NumerPokoju' => $this->getRoomNumber(),
                 'TypPokoju' => $this->getRoomType(),
                 'Pietra_idPietra' => $this->getFloorID(),
-                'X' => $this->getX(),
-                'Y' => $this->getY(),
-                'X2' => $this->getX2(),
-                'Y2' => $this->getY2(),
             ]);
 
             $this->setRoomID($pdo->lastInsertId());
         } else {
-            $sql = "UPDATE pokoje SET NumerPokoju = :NumerPokoju, TypPokoju = :TypPokoju, Pietra_idPietra = :Pietra_idPietra, X = :X, Y = :Y, X2 = :X2, Y2 = :Y2 WHERE idPokoje = :idPokoje";
+            $sql = "UPDATE pokoje SET NumerPokoju = :NumerPokoju, TypPokoju = :TypPokoju, Pietra_idPietra = :Pietra_idPietra WHERE idPokoje = :idPokoje";
             $statement = $pdo->prepare($sql);
             $statement->execute([
                 ':idPokoje' => $this->getRoomID(),
                 ':NumerPokoju' => $this->getRoomNumber(),
                 ':TypPokoju' => $this->getRoomType(),
                 ':Pietra_idPietra' => $this->getFloorID(),
-                ':X' => $this->getX(),
-                ':Y' => $this->getY(),
-                ':X2' => $this->getX2(),
-                ':Y2' => $this->getY2(),
             ]);
         }
     }
@@ -200,9 +140,5 @@ class Rooms{
         $this->setRoomNumber(null);
         $this->setRoomType(null);
         $this->setFloorID(null);
-        $this->setX(null);
-        $this->setY(null);
-        $this->setX2(null);
-        $this->setY2(null);
     }
 }
