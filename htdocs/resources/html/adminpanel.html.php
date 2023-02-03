e528eb2e14282818ddcbcfc77fae5ac39f3bcb76
Neikar <32700210+Neikar@users.noreply.github.com>
2023-02-03 10:21:49 +0100

    Update adminpanel.html.php

--- htdocs/resources/html/adminpanel.html.php
+++ htdocs/resources/html/adminpanel.html.php
@@ -46,6 +46,162 @@
 	
 </div>
 
+
+<div class="col-6">
+	<h2>Formularz Dodania Pliku</h2>
+	<form action="index.php" method="post">
+		<label> Dodaj / Usuń plik </label>
+		<input type="submit" name="page" value="Formularz Plików">
+	</form>
+</div>
+
+
+<div class="col-6">
+
+	<h2>Lista budynków</h2>
+
+	<br />
+
+	<table>
+
+		<tr>
+			<th>Numer budynku</th>
+			<th>Adres budynku</th>
+			<th>Opis</th>
+			<th>Lat</th>
+			<th>Lng</th>
+			<th>Akcja</th>
+		</tr>
+		<?php 
+		
+		foreach($HTML_BUDYNKI as $budynek):
+		
+		?>
+		<tr>
+			<td>		
+				<?=$budynek['NumerBudynku']?>	
+			</td>
+			<td>
+				<?=$budynek['AdresBudynku']?>
+			</td>
+			<td>
+				<?=$budynek['OpisDodatkowy']?>
+			</td>
+			<td>
+				<?=$budynek['SzerokoscGeo']?>
+			</td>
+			<td>
+				<?=$budynek['DlugoscGeo']?>
+			</td>
+			<td>
+				<form action="" method="post">
+					<input type="hidden" name="idBudynki" value="<?=$budynek['idBudynki']?>">
+					<input type="submit" name="usun_budynek" value="usun">
+				</form>
+			</td>
+		</tr>
+		<?php
+		
+		endforeach;
+		
+		?>
+</table>	
+	
+</div>
+
+
+<div class="col-6">
+
+	<h2>Lista pięter</h2>
+
+	<br />
+
+	<table>
+
+		<tr>
+			<th>Numer pietra</th>
+			<th>Id Budynek</th>
+			<th>Opis</th>
+		
+		</tr>
+		<?php 
+		
+		foreach($HTML_PIETRA as $pietro):
+		
+		?>
+		<tr>
+			<td>		
+				<?=$pietro['NumerPietra']?>	
+			</td>
+			<td>
+				<?=$pietro['Budynki_idBudynki']?>	<?php //$HTML_BUDYNKI[$pietro['Budynki_idBudynki']-1]['OpisDodatkowy'];?>
+			</td>
+			<td>
+				<?=$pietro['PhotoUrl']?>
+			</td>
+			<td>
+				<form action="" method="post">
+					<input type="hidden" name="idPietra" value="<?=$pietro['idPietra']?>">
+					<input type="submit" name="usun_pietro" value="usun">
+				</form>
+			</td>
+		</tr>
+		<?php
+		
+		endforeach;
+		
+		?>
+</table>	
+	
+</div>
+
+<div class="col-6">
+
+	<h2>Lista pomieszczeń</h2>
+
+	<br />
+
+	<table>
+
+		<tr>
+			<th>Numer Pokoju</th>
+			<th>Typ Pokoju</th>
+			<th>id Pietra</th>
+		
+		</tr>
+		<?php 
+		
+		foreach($HTML_POKOJE as $pomieszczenie):
+		
+		?>
+		<tr>
+			<td>		
+				<?=$pomieszczenie['NumerPokoju']?>	
+			</td>
+			<td>
+				<?=$pomieszczenie['TypPokoju']?>	
+			</td>
+			<td>
+				<?=$pomieszczenie['Pietra_idPietra']?>
+			</td>
+			<td>
+				<form action="" method="post">
+					<input type="hidden" name="idPokoje" value="<?=$pomieszczenie['idPokoje']?>">
+					<input type="submit" name="usun_pokoj" value="usun">
+				</form>
+			</td>
+		</tr>
+		<?php
+		
+		endforeach;
+		
+		?>
+</table>	
+	
+</div>
+
+
+
 <div class="col-6">
 	<h2>Dodaj pracownika</h2>
 	<br />
@@ -58,9 +214,69 @@
 
 </div>
 <div class="col-6">
-	<h2>Formularz Dodania Pliku</h2>
+	<h2>Dodaj budynek</h2>
+	<br />
 	<form action="index.php" method="post">
-		<label> Dodaj / Usuń plik </label>
-		<input type="submit" name="page" value="Formularz Plików">
+		Numer budynku: <input type="number"  name="NumerBudynku"><br />
+		Adres budynku: <input type="text" name="AdresBudynku"><br />
+		Opis: <input type="text" name="OpisDodatkowy"><br />
+		Lat: <input type="number" step=any name="SzerokoscGeo"><br />
+		Lng: <input type="number" step=any name="DlugoscGeo"><br />
+		<input type="submit" name="dodaj_budynek" value="dodaj">
 	</form>
+
+</div>
+<div class="col-6">
+	<h2>Dodaj pietro</h2>
+	<br />
+	<form action="index.php" method="post">
+		Numer pietra: <input type="number"  name="NumerPietra"><br />
+		
+		Budynek:<select name="Budynki_idBudynki">
+		<?php 
+		foreach($HTML_BUDYNKI as $budynek):
+		?>
+		<option value="<?=$budynek['idBudynki']?>"><?=$budynek['OpisDodatkowy']?></option>
+		
+		<?php
+		
+		endforeach;
+		
+		?>
+		</select><br />
+		
+		
+		<input type="submit" name="dodaj_pietro" value="dodaj">
+	</form>
+
+</div>
+<div class="col-6">
+	<h2>Dodaj pomieszczenie</h2>
+	<br />
+	<form action="index.php" method="post">
+		Numer Pokoju: <input type="text"  name="NumerPokoju"><br />
+		
+		Typ pomieszczenia:<select name="TypPokoju">
+		<option value="1">1</option>
+		<option value="2">2</option>
+		
+		</select><br />
+		
+		Pietro:<select name="Pietra_idPietra">
+		<?php 
+		foreach($HTML_PIETRA as $pietro):
+		?>
+		<option value="<?=$pietro['idPietra']?>"><?=$pietro['NumerPietra']?> (Budynek Id:<?=$pietro['Budynki_idBudynki']?>)</option>
+		
+		<?php
+		
+		endforeach;
+		
+		?>
+		</select><br />
+		
+		
+		<input type="submit" name="dodaj_pokoj" value="dodaj">
+	</form>
+
 </div>
